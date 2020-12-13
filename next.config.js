92be92c78198b2fdf57062d6f91fff947f65bdc8
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-12-14 00:56:26 +0100

    api preview

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 const withTM = require('next-transpile-modules')(['eventjuicer-site-components']);
 
-  module.exports = withTM({
+module.exports = withTM({
     webpack: (config) => {
-      config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
-      return config
-    },
 
+      config.resolve.alias['react'] = path.resolve(__dirname, 'node_modules', 'react');
+      config.resolve.alias['react-dom'] = path.resolve(__dirname,'node_modules', 'react-dom');
+
+      return config 
+    },
     i18n: {
       locales: ['pl'],
       defaultLocale: 'pl',
-      domains: [{
-          domain: 'ehandel.com.pl',
-          defaultLocale: 'pl',
-      }],
     },
 });
