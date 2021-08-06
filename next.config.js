54bd4e8043ddbe4b69d189be6c0ef4522264338e
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-06 13:09:40 +0200

    added sentry for logging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 const path = require('path');
 const withTM = require('next-transpile-modules')(['eventjuicer-site-components']);
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withTM({
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+
+module.exports = withSentryConfig(withTM({
     webpack: (config) => {
 
       config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
@@ -22,4 +36,10 @@ module.exports = withTM({
       ]
     },
 
-});
+}), SentryWebpackPluginOptions);
+
+
+
+
+
+
